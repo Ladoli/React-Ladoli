0dfca2940375c4c6ae6e3aa66dd765ff16338deb
Ladoli <villadolidangeloryan@gmail.com>
2018-05-24 20:02:03 -0700

    Shortcut icon, deployment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './CSS/ViStyles.css';
 import './CSS/ViRecurring.css';
 import Home from './Home/Home';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><Home /></Router>, document.getElementById('root'));
+
 registerServiceWorker();
