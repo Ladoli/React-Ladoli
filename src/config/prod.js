424b1787b82e0373d543aa6f0083732890bf5be1
Ladoli <villadolidangeloryan@gmail.com>
2020-01-29 00:23:46 -0800

    Added explaination for exposed key

--- src/config/prod.js
+++ src/config/prod.js
@@ -3,3 +3,8 @@ export const FirebaseConfig = {
   authDomain: "Ladoli-Portfolio.firebaseapp.com",
   databaseURL: "ladoli-portfolio.firebaseio.com/"
 };
+
+//This API key is just an identifier for the project, me exposing it is not a security risk. 
+//Firebase security is done through database rules and authorized domains.
+//If this was an actually an API key I needed to keep secure, I would just put it in an environment variable
+//https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/37484053#37484053
